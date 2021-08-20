450d7a5a2b61f05622ce7efeec80a433387ab09a
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2021-08-19 22:03:59 -0300

    Ajuste e tratamentos de erros.

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Via-CEP-API-Consult
-Script for querying an api that returns data from Brazilian zip codes
+Script for querying an api that returns data from Brazilian zip codes.
+
+Lib Documentation: [Requests: HTTP for Humans™](https://docs.python-requests.org/en/master/)
 
 ## Comands 
 
-Instalation virtual enviriot 
-
+### Create virtual environment 
+```
 python -m venv .venv
+```
+## Activate virtual environment 
+```
+#source virtualenv_name/bin/activate
+source .venv/bin/activate
+```
 
+### Installation of the library
+```
 pip install requests
+```
+### Run script
+```
+python cep_consult.py
+```
--- cep_consult.py
+++ cep_consult.py
@@ -2,25 +2,35 @@ import requests
 
 class ViaCepApi:
     def __init__(self):
-        print('Olá!\nSeja bem vindo.\nEu me chamo Via CEP, vou retornar dados de algum CEP brasileiro que você me passar!')
-        cep = str(input('\nEstou a sua disposição!\nInsira o CEP aqui: '))
-        data = self.validateCEP(cep)
-        self.consultarCep(data)
+        print('Olá!\nSeja bem vindo.\nEu me chamo Cepinho, vou te trazer dados de algum CEP brasileiro que você me passar!')
+        try:
+            consultar = requests.get('https://www.google.com/')
+            if (consultar.status_code == 200):
+                return self.consultarCep()
+        except:
+            print('Parece que estamos sem Internet.\nVerifique a sua conexão e tente novamente!')
 
-    def consultarCep(self, cep):
-        print(f'https://viacep.com.br/ws/{cep}/json/')
+    def consultarCep(self):
+        cep = str(input('Informe o código do CEP: '))
+        while(cep == None or cep == ""):
+            cep = str(input('Por gentileza informe o código do CEP: '))
         request = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
         if request.status_code != 200:
-            print('Algo deu errado!\nVerifique sua conexão com a internet e tente mais tarde.')
+            if(len(cep) != 8 or not cep.isdigit()):
+                return print('Verifique se você está inserido o CEP corretamente!')
+            print('Algo deu errado!\nTente novamente mais tarde.')
         elif request.status_code == 200:
             print("Deu certinho!")
             print(request.json())
 
     def validateCEP(self, cep):
-        #data = cep.replace('-', '')
+        cep = cep.replace('-', '')
         if type(cep) != str:
-            print(f'{cep} - Esse não é um CEP válido!\nPor gentileza corriga e tente novamente.')
+            print('Esse não é um CEP válido!\nPor gentileza corriga e tente novamente.')
+            replay = int(input("Deseja tentar novamente?\n0 = Não - 1 = Sim"))
+            while(replay == 1):
+                self.consultarCep(cep)
         else:
             return cep
 
-chamarApi = ViaCepApi()
+executar = ViaCepApi()
